410369adf526461e5aa9bb81fb1308afe4bd6896
Kauan Ferreira <ksferreira11@gmail.com>
2023-12-10 17:40:50 -0500

    Remove footer

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
         </Routes>
       </main>
       <footer>
-        <Footer />
+        {/* <Footer /> */}
       </footer>
 
       </ParallaxProvider>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,13 +8,12 @@ const Footer = () => {
         sx={{
             bgcolor: CONSTANTS.ZBT_DARK_NAVY,
             width: '100%',
-            display: 'inline-block',
-            textAlign: 'center',
+            display: 'flex',
+            justifyContent: 'center',
             position: 'relative',
             margin: 0,
-        }}
-        component={'div'}>
-            <Typography variant='h1'>
+        }}>
+            <Typography variant='h4'>
                 ZETA BETA TAU FOOTER
             </Typography>
     </Box>
